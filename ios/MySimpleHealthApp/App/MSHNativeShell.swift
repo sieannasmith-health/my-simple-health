f871448c5684b0652bb3efcab4a17340a6160ece
sieannasmith-health <hello.mysimplehealth@gmail.com>
2026-09-02 00:08:20 -0400

    Make interpreted My Health home the first tab

--- ios/MySimpleHealthApp/App/MSHNativeShell.swift
+++ ios/MySimpleHealthApp/App/MSHNativeShell.swift
@@ -194,7 +194,7 @@ private struct MSHSectionNavigation: View {
                         if let notificationRoute {
                             MSHNotificationWebRouteScreen(route: notificationRoute)
                         } else {
-                            MSHMyHealthScreen()
+                            MSHMyHealthHomeScreen()
                         }
                     case .calendar:
                         if let notificationRoute {
